b7afc540077c4c97305ee34f2b6953758602b3e4
Thad <Thadphillp35@gmail.com>
2026-03-06 12:34:23 -0500

    I think i am done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Form submitted!");
+});
+document.querySelector("form").addEventListener("reset", function(event) {
+    alert("Form reset!");
+});
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Form submitted!");
+});
